8009964281858f697d3f0448c024c88c8a77652f
Leonardo Rochael Almeida <leorochael@gmail.com>
2019-10-18 17:48:22 -0300

    fixup! WIP install firefox
    Remap current user UID to container 1000.
    
    Export Downloads dir and .Xauthority into the container.
    
    Run firefox by default on docker launch.

--- run.sh
+++ run.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 set -x
-podman run --rm -ti -v /dev/shm:/dev/shm -v /tmp:/tmp --env=DISPLAY --net=host warsaw-banking
+DOWNLOAD_PATH=$(xdg-user-dir DOWNLOAD)/warsaw-banking
+mkdir -p "$DOWNLOAD_PATH"
+
+run_container () {
+    podman run --rm -ti \
+        -v /dev/shm:/dev/shm -v /tmp:/tmp \
+        -v "$DOWNLOAD_PATH":/home/ubuntu/Downloads -v ${XAUTHORITY:-~/.Xauthority}:/home/ubuntu/.Xauthority \
+        --env=DISPLAY \
+        --uidmap=0:1:999 --uidmap=1000:0:1 --uidmap=1001:1000:64536 \
+        --gidmap=0:1:999 --gidmap=1000:0:1 --gidmap=1001:1000:64536 \
+        --net=host \
+        warsaw-banking "$@"
+}
+
+if [ $# -eq 0 ]; then
+    run_container firefox --no-remote
+else
+    run_container "$@"
+fi

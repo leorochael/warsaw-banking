25cc4d2808eea9e07a358333e7383ea2d67a8df1
Leonardo Rochael Almeida <leorochael@gmail.com>
2020-02-11 00:00:56 -0300

    Reformat

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -x
-podman run --rm -ti -v /dev/shm:/dev/shm -v /tmp:/tmp --env=DISPLAY --env=XAUTH_COOKIE="$(bash -c 'echo $2' $(xauth list $DISPLAY|head -1))" --net=host warsaw-banking
+podman run --rm -ti \
+    -v /dev/shm:/dev/shm \
+    -v /tmp:/tmp \
+    --net=host \
+    --env=DISPLAY \
+    --env=XAUTH_COOKIE="$(bash -c 'echo $2' $(xauth list $DISPLAY|head -1))" \
+    warsaw-banking
